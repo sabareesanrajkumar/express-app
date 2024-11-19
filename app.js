6b7b7898ee6969146abf75c653c5cebf3f501f82
sabareesanrajkumar <145452371+sabareesanrajkumar@users.noreply.github.com>
2024-11-19 16:06:08 +0530

    Add files via upload
    handling routes and using body parser

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(`/add-product`, (req, res, next) => {
+  res.send(`
+    <h1>Add product</h1>
+    <form action="/product" method="POST">
+      <input type="text" name="titile" placeholder="product">
+      <input type="number" name="size" placeholder="size">
+      <button type = "submit">Add</button>
+    </form`);
 });
 
-app.use((req, res, next) => {
-  console.log("in another middleware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/add-product");
+});
+
+app.use(`/`, (req, res, next) => {
   res.send(`<h1>Hello from express</h1>`);
 });
 
