3aafb2ab3f3600629ac9a8cccaa1dc8a52903704
sabareesanrajkumar <145452371+sabareesanrajkumar@users.noreply.github.com>
2024-11-19 13:17:05 +0530

    Add files via upload
    using express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware");
+  res.send(`<h1>Hello from express</h1>`);
+});
+
+app.listen(3000);
